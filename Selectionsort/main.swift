108a85c27179225f4d8dceaad60cd3f9d54e5a92
Unstoppable20171103191 <1163681662@qq.com>
2018-10-12 20:02:40 +0800

    Initial Commit

---
+++ Selectionsort/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Selectionsort
+//
+//  Created by s20171103191 on 2018/10/12.
+//  Copyright © 2018年 s20171103191. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
